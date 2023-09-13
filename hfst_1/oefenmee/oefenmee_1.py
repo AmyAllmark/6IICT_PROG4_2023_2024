7061592fb0a79caca413b2ddce96bd3565ced555
AmyAllmark <amyallmark3@gmail.com>
2023-09-13 08:55:42 +0200

    commit

---
+++ hfst_1/oefenmee/oefenmee_1.py
@@ -0,0 +1,5 @@
+dieren= {#sleutel is dieren, waarde is aantal
+    "paard": 6,
+    "hond": 3,
+    "eend":5,
+}
--- hfst_1/oefenmee/oefenmee_2.py
+++ hfst_1/oefenmee/oefenmee_2.py
@@ -4,3 +4,22 @@ fruitmand = { # Sleutel is fruit, waarde is aantal
     "banaan": 3,
     "kers": 50
 }
+# Print de dictionary-waarde gekoppeld aan onderstaande variabele
+fruit = "banaan"
+print( fruitmand[fruit] )
+
+nieuw_fruit  = "mango"
+nieuw_aantal1 = 1
+fruitmand[nieuw_fruit]= nieuw_aantal1
+
+
+fruit = "banaan"
+nieuw_aantal2 = 8
+fruitmand[fruit]= nieuw_aantal2
+terugleggen_fruit = "kers"
+fruitmand.pop("kers") # kers verwijderen uit lijst
+print(fruitmand)
+
+
+
+
