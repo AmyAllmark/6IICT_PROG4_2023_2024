bc6d13896b2013413e3194c7950c0dcf1325d012
AmyAllmark <amyallmark3@gmail.com>
2024-01-10 09:51:00 +0100

    oefening 12

--- hfst_4/oefenmee/oefenmee_12.py
+++ hfst_4/oefenmee/oefenmee_12.py
@@ -1,10 +1,18 @@
 def deel(teller, noemer):
+    if type(teller) != int:
+        raise TypeError( "teller is geen int")
+
+    if type(noemer)!= int:
+        raise TypeError( "noemer is geen int")
+
+    if noemer == 0:
+        raise ZeroDivisionError("Snul.")
     return teller/noemer
 
-" Voorbeelden om de werking van functie deel te testen. "
-# print( deel(8, 2) )      # 4.0
+    
+print( deel(8, 2) )      # 4.0
 
-# print( deel(9, 0) )      # ZeroDivisionError: Snul.
-# print( deel(True, 4) )   # TypeError: True is <class 'bool'>, moet int/float zijn.
-# print( deel(8, "a")  )   # TypeError: a is <class 'str'>, moet int/float zijn.
-# print( deel(True, "a") ) # TypeError: True is <class 'bool'>, moet int/float zijn.
+#print( deel(9, 0) )      # ZeroDivisionError: Snul.
+#print( deel(True, 4) )   # TypeError: True is <class 'bool'>, moet int/float zijn.
+print( deel(8, "a")  )   # TypeError: a is <class 'str'>, moet int/float zijn.
+print( deel(True, "a") ) # TypeError: True is <class 'bool'>, moet int/float zijn.
