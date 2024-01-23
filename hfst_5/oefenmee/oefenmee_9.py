5589bf7427ae7a30d54794f6bc0a9d084590a24e
AmyAllmark <amyallmark3@gmail.com>
2024-01-23 08:27:14 +0100

    oefeningen

--- hfst_5/oefenmee/oefenmee_9.py
+++ hfst_5/oefenmee/oefenmee_9.py
@@ -1,7 +1,27 @@
 # Maak de klasse Hond aan zoals omschreven in oefen mee 9.
+import random
+
+locaties = ["living", "tuin", "buren", "keuken"]
+
+class Hond:
+    def __init__(self,naam):
+        self.naam=naam
+        self.locatie = random.choice(locaties)
+
+    def ziet_hond(self, andere_hond: 'Hond'):
+        if self.locatie == andere_hond.locatie:
+            print(f'{self.naam} ziet {andere_hond.naam} in de {self.locatie}')
+            self.locatie = random.choice(locaties)
+
+            while True:
+                nieuwe_locatie=random.choice(locaties)
+                if nieuwe_locatie != self.locatie:
+                    self.locatie = nieuwe_locatie
+                    break
 
 
-
+            print(f"{self.naam} is bang en rent naar de {random.choice(locaties)}")
+    
 " Via onderstaande code kan je niveau 1 testen. "
 
 # hond_1 = Hond("Lulu", "keuken")
@@ -15,11 +35,11 @@
 " Via onderstaande code kan je niveau 2 & 3 testen. Opgelet! Resultaat is random. "
 " Best print je in __init__ ook de locatie waar iedere hond start, zo kan je de werking makkelijker nagaan. "
 
-# hond_1 = Hond("Lulu")
-# hond_2 = Hond("Floris")
-# hond_3 = Hond("Ranger")
+hond_1 = Hond("Lulu")
+hond_2 = Hond("Floris")
+hond_3 = Hond("Ranger")
 
-# hond_1.ziet_hond(hond_2)                  
-# hond_1.ziet_hond(hond_3)   
+hond_1.ziet_hond(hond_2)                  
+hond_1.ziet_hond(hond_3)   
           
-# print(hond_1.locatie)   
+print(hond_1.locatie)   
--- hfst_5/opdrachten/opdracht_1.py
+++ hfst_5/opdrachten/opdracht_1.py
@@ -2,24 +2,41 @@
 
 class Familie:
     ras = "mens"# omdat dit altijd zo is en de zelfde waarde zal moeten hebben
-    def __init__(self, geslacht, leeftijd,naam) :
+    def __init__(self, geslacht, leeftijd,naam,haar) :
         self.geslacht=geslacht
         self.leeftijd=leeftijd
         self.naam=naam
+        self.haar=haar
 
     def is_leeftijd(self):
+        return self.leeftijd
+   
     
     def is_geslacht(self):
+        return self.geslacht
+    
+    def is_naam(self):
+        return self.naam
+    
+    def verf_haar(self, nieuwe_kleur):
+        self.haar = nieuwe_kleur
+        print(f'{self.naam} is nu {self.haar}')
 
-        
+    def verjaardag(self):
+        self.leeftijd=self.leeftijd+1
+        print(f'gefeliciteerd {self.naam} je bent nu {self.leeftijd}')
 
+    def uitleg(self):
+        print(f" {self.naam}: {self.geslacht}, {self.leeftijd} ,{self.haar}, {self.ras}")
+
+  
 
 " Via onderstaande code kan je de klasse Familie testen. "
 
-# vader = Familie(geslacht="man", haar="zwart", leeftijd=39, naam="Steve")
-
-# print(vader.is_leeftijd())  # 39
-# print(vader.is_geslacht())  # man
-# vader.verjaardag()          # Gefeliciteerd Steve, je bent nu 40
-# vader.verf_haar("blond")    # Steve is nu blond
-# vader.uitleg()              # Steve: man, 40, blond, mens
+vader = Familie(geslacht="man", haar="zwart", leeftijd=39, naam="Steve")
+print(vader.is_leeftijd())  # 39
+print(vader.is_geslacht())  # man
+vader.verjaardag()          # Gefeliciteerd Steve, je bent nu 40
+vader.verf_haar("blond")    # Steve is nu blond
+print(vader.haar)
+vader.uitleg()              # Steve: man, 40, blond, mens
